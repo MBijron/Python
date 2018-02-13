e7d74039756ad4728e998f1c2f940374c75a5073
Maurice <riciex@hotmail.com>
2018-02-13 15:38:32 +0100

    Removed setFont from the terminal start arguments for performance reasons

--- commands/terminal.py
+++ commands/terminal.py
@@ -12,8 +12,7 @@ class TerminalCommand (CommandBase):
 
     def main(self):
         ansicon_path = ExtraWorks.install_if_missing("ansicon@1.81") + "\\ansicon.exe"
-        setfont_path = ExtraWorks.install_if_missing("setfont@1.0") + "\\setfont.exe"
-        call(["cmd", "/K", ansicon_path, "-p", "&&", setfont_path, "Consolas", "0", "16", "&&", "set", "PROMPT=$E[0;36m%username%+ - $P$_$E[36;1m$G$E[0m$S", "&&", "set",  "nl=^&echo.", "&&", "title", "terminal"])
+        call(["cmd", "/K", ansicon_path, "-p", "&&", "set", "PROMPT=$E[0;36m%username%+ - $P$_$E[36;1m$G$E[0m$S", "&&", "set",  "nl=^&echo.", "&&", "title", "terminal"])
 
 
 # If the file is called directly (not imported) execute the command.
